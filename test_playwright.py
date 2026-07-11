021f25e4414de15fc09e68646a00af47cff8c10b
Mahery Dirou <160710781+maherydirou@users.noreply.github.com>
2026-07-11 11:42:35 +0200

    Update test_playwright.py

--- test_playwright.py
+++ test_playwright.py
@@ -1,34 +1,19 @@
 from playwright.sync_api import sync_playwright
 
-responses = []
-
 with sync_playwright() as p:
-
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
-    def handle_response(response):
-        if "/api/Offers/search" in response.url:
-            print("API détectée :", response.url)
-            responses.append(response)
-
-    page.on("response", handle_response)
+    page.on(
+        "request",
+        lambda request: print(request.method, request.url)
+    )
 
     page.goto(
         "https://mon-vie-via.businessfrance.fr/offres",
         wait_until="networkidle"
     )
 
-    # On laisse le temps au JavaScript de finir ses appels
     page.wait_for_timeout(5000)
-
-    print("Nombre de réponses API :", len(responses))
-
-    if responses:
-        data = responses[-1].json()
-
-        print("Nombre d'offres :", len(data["result"]))
-        print("Première offre :")
-        print(data["result"][0])
 
     browser.close()

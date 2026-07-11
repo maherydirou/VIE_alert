7fc94805249b857d38dda1388c06ca6823598649
Mahery Dirou <160710781+maherydirou@users.noreply.github.com>
2026-07-11 11:37:18 +0200

    Refactor test_playwright.py for better response handling
    Refactor Playwright test to improve response handling and logging.

--- test_playwright.py
+++ test_playwright.py
@@ -3,33 +3,27 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
-
     page = browser.new_page()
-
-    response_json = None
-
-    def intercept(response):
-        global response_json
-
-        if "/api/Offers/search" in response.url:
-
-            print("API trouvée :", response.url)
-
-            try:
-                data = response.json()
-
-                print("Nombre d'offres :", len(data["result"]))
-
-                print(data["result"][0])
-
-            except Exception as e:
-                print(e)
-
-    page.on("response", intercept)
 
     page.goto(
         "https://mon-vie-via.businessfrance.fr/offres",
-        wait_until="networkidle"
+        wait_until="domcontentloaded"
     )
+
+    print("Page chargée")
+
+    response = page.wait_for_response(
+        lambda r: "/api/Offers/search" in r.url,
+        timeout=30000
+    )
+
+    print("Réponse reçue :", response.status)
+
+    data = response.json()
+
+    print("Nombre d'offres :", len(data["result"]))
+
+    print("Première offre :")
+    print(data["result"][0])
 
     browser.close()

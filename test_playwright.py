dfc853566321d4752b19e5832733e37cff71ada4
Mahery Dirou <160710781+maherydirou@users.noreply.github.com>
2026-07-11 11:35:22 +0200

    Update test_playwright.py

--- test_playwright.py
+++ test_playwright.py
@@ -6,11 +6,30 @@ with sync_playwright() as p:
 
     page = browser.new_page()
 
+    response_json = None
+
+    def intercept(response):
+        global response_json
+
+        if "/api/Offers/search" in response.url:
+
+            print("API trouvée :", response.url)
+
+            try:
+                data = response.json()
+
+                print("Nombre d'offres :", len(data["result"]))
+
+                print(data["result"][0])
+
+            except Exception as e:
+                print(e)
+
+    page.on("response", intercept)
+
     page.goto(
         "https://mon-vie-via.businessfrance.fr/offres",
         wait_until="networkidle"
     )
-
-    print(page.title())
 
     browser.close()
